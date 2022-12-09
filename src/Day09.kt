a9b0d313575a8b9f570c2c1c88afd00581f5a101
Rob van der Most <Rob.vanderMost@TomTom.com>
2022-12-09 09:53:47 +0100

    feat: solve day 9

---
+++ src/Day09.kt
@@ -0,0 +1,114 @@
+import kotlin.math.abs
+
+private data class Coordinate(
+    val x: Int,
+    val y: Int
+) {
+    fun moveUp() = Coordinate(x, y+1)
+    fun moveDown() = Coordinate(x, y-1)
+    fun moveLeft() = Coordinate(x-1, y)
+    fun moveRight() = Coordinate(x+1, y)
+
+    fun isTouching(other: Coordinate): Boolean {
+        val deltaX = abs(other.x - x)
+        val deltaY = abs(other.y - y)
+        return (deltaY < 2 && deltaX < 2)
+    }
+    fun follow(other: Coordinate) =
+        if (isTouching(other)) {
+            this
+        } else {
+            let {
+                when  {
+                    other.x > it.x -> it.moveRight()
+                    other.x < it.x -> it.moveLeft()
+                    else -> it
+                }
+            }
+                .let {
+                    when {
+                        other.y > it.y -> it.moveUp()
+                        other.y < it.y -> it.moveDown()
+                        else -> it
+                    }
+                }
+        }
+
+}
+
+private data class Day09Instruction(
+    val direction: Char,
+    val amount: Int
+) {
+    companion object {
+        fun fromString(input: String) =
+            Day09Instruction(input[0], input.substring(2).toInt())
+    }
+}
+fun main() {
+    fun part1(input: List<String>): Int {
+        var head = Coordinate(0, 0)
+        var tail = Coordinate(0, 0)
+        val tailPositions = mutableSetOf(tail)
+
+        input
+            .map { Day09Instruction.fromString(it) }
+            .forEach {
+                (1..it.amount).forEach { _ ->
+                    head = when (it.direction) {
+                        'R' -> head.moveRight()
+                        'L' -> head.moveLeft()
+                        'U' -> head.moveUp()
+                        'D' -> head.moveDown()
+                        else -> throw RuntimeException("Invalid direction")
+                    }
+                    tail = tail.follow(head)
+                    tailPositions.add(tail)
+                }
+            }
+        return tailPositions.size
+    }
+
+    fun part2(input: List<String>): Int {
+        var rope = buildList {
+            (0..9).forEach { _ -> add(Coordinate(0,0))}
+        }
+        return input
+            .map { Day09Instruction.fromString(it) }
+            .flatMap {instruction ->
+                List(instruction.amount) { instruction.direction }
+            }
+            .scan(rope) { r, d ->
+                buildList {
+                    val h = when (d) {
+                        'R' -> r.first().moveRight()
+                        'L' -> r.first().moveLeft()
+                        'U' -> r.first().moveUp()
+                        'D' -> r.first().moveDown()
+                        else -> throw RuntimeException("Invalid direction")
+                    }
+                    add(h)
+                    r.drop(1).scan(h) { prev, current ->
+                        val t = current.follow(prev)
+                        add(t)
+                        t
+                    }
+                }
+            }
+            .map { it.last() }
+            .toSet()
+            .size
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day09_test")
+    check(part1(testInput) == 13)
+    check(part2(testInput) == 1)
+
+    val testInput2 = readInput("Day09_test2")
+    check(part2(testInput2) == 36)
+
+    val input = readInput("Day09")
+    println(part1(input))
+    println(part2(input))
+}
